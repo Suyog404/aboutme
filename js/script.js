6f424c02aecf9ebc5333323be3184c386cb5014a
Suyog404 <78721202+Suyog404@users.noreply.github.com>
2022-05-17 19:03:34 +0545

    Website Upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*========typing animation ======*/
+var typed = new Typed(".typing",{
+    strings:["Computer Engineer","Software Developer","Data Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var min= new Typed(".minor",{
+    strings:["Minor project: An ecommerce web application for sports and fitness products, built using React, Node.js, Express.js, MongoDB and Paypal Sandbox for payment gateway. It is basically a MERN-Stack project. This web applciation contains some features like CRUDE operation, Recomendation System and Sorting "],
+    typeSpeed:40,
+    BackSpeed:50,
+    loop:true
+
+})
+var maj = new Typed(".major",{
+    strings:["Major project: Detection of Covid-19 from Chest X-ray imaging datasets from various machine learning Algortihms: Support Vector Machine, Logistic Regression and Random Forest and making a comparitive analysis among the mentioned machine learning algortihms. It is basically a image processing project."],
+    typeSpeed:40,
+    BackSpeed:50,
+    loop:true
+})
+
+
